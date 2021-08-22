d450f9abefd41f0e313be7c60b109bd49731b5b5
VaxobjanovDev <vaxobjanovdev@gmail.com>
2021-08-22 16:10:54 +0500

    Simle Calculator

---
+++ src/KeyBoard.js
@@ -0,0 +1,30 @@
+const KeyboardOb = [
+  {
+    name: "/",
+    className: "bg",
+  },
+  { name: "7" },
+  { name: "8" },
+  { name: "9" },
+  {
+    name: "*",
+    className: "bg",
+  },
+  { name: "4" },
+  { name: "5" },
+  { name: "6" },
+  {
+    name: "-",
+    className: "bg"
+  },
+  { name: "1" },
+  { name: "2" },
+  { name: "3" },
+  {
+    name: "+",
+    className: "bg",
+  },
+  { name: "0" },
+  { name: "." },
+];
+export default KeyboardOb;
---
+++ src/Main.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import KeyboardOb from "./KeyBoard";
+import "./Main.css";
+
+const Main = () => {
+	const [result, setResult] = useState("");
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name));
+  };
+
+  const clear = () => {
+    setResult("");
+  };
+
+  const backspace = () => {
+    setResult(result.slice(0, -1));
+  };
+
+  const calculator = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (err) {
+      setResult("Error");
+    }
+  };
+
+  return (
+    <div className="container">
+      <form>
+        <input autoFocus='on' className="input" value={result} />
+      </form>
+      <div className="keyboard">
+        <button className="clear" onClick={clear}>
+          Clear
+        </button>
+        <button className="backspace" onClick={backspace}>
+          C
+        </button>
+        {KeyboardOb.map((item) => {
+          return (
+            <button
+              className={item.className}
+              onClick={handleClick}
+              name={item.name}
+            >
+							{item.name}
+            </button>
+          );
+        })}
+        <button className="equal" onClick={calculator}>
+          =
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Main
